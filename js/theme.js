e4ea51bd431757bdaaef88d5b5baa72c5d841d9b
Suparna Paul <suparnapaul393@gmail.com>
2018-07-23 19:10:27 +0530

    Add files via upload

---
+++ js/theme.js
@@ -0,0 +1,37 @@
+// jQuery to collapse the navbar on scroll
+$(window).scroll(function() {
+    if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
+});
+
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+// Closes the Responsive Menu on Menu Item Click
+$('.navbar-collapse ul li a').click(function() {
+    $('.navbar-toggle:visible').click();
+});
+
+
+$(document).ready(function() {
+    $('#easing').lightGallery({
+        cssEasing:'cubic-bezier(0.680, -0.550, 0.265, 1.550)',
+        closable:false,
+        enableTouch: false,
+        enableDrag: false,
+        loop:true,
+        speed:1500
+    }); 
+});
+
